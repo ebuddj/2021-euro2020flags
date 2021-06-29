e6fad719c04015e1b516d7e3fed8855a073bf58a
Teemo Tebest <teemo.tebest@gmail.com>
2021-06-29 23:37:37 +0200

    Removed flags

--- src/jsx/App.jsx
+++ src/jsx/App.jsx
@@ -3,11 +3,10 @@ import style from './../styles/styles.less';
 
 // https://www.npmjs.com/package/country-flags-svg
 // https://www.npmjs.com/package/round-flags
-import { AT,BE,CH,DE,DK,ES,FR,HR,IT,PT,SE,NL,CZ,UA } from 'round-flags';
+import { BE,CH,DE,DK,ES,IT,SE,CZ,UA } from 'round-flags';
 
 // https://svgcrop.com/
 import GB_EN from './../../media/img/uk-en-circle-01.png';
-import GB_WL from './../../media/img/uk-wl-circle-01.png';
 
 class App extends Component {
   constructor(props) {
@@ -35,20 +34,12 @@ class App extends Component {
       <div className={style.app}>
         <div>
           <img src={IT} alt="Italy" />
-          <img src={AT} alt="Austria" />
           <img src={BE} alt="Belgium" />
-          <img src={PT} alt="Portugal" />
-          <img src={HR} alt="Croatia" />
           <img src={ES} alt="Spain" />
-          <img src={FR} alt="France" />
           <img src={CH} alt="Switzerland" />
-          <img src={GB_WL} alt="Wales" />
           <img src={DK} alt="Denmark" />
-          <img src={NL} alt="Netherlands" />
           <img src={CZ} alt="Czechia" />
-          <img src={GB_EN} alt="England" />
-          <img src={DE} alt="Germany" />
-          <img src={SE} alt="Sweden" />
+          <img src={GB_EN} alt="Ukraine" />
           <img src={UA} alt="Ukraine" />
         </div>
       </div>
